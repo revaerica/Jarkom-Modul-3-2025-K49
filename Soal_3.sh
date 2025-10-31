347f7b586a54714e095d47c881127f931fc1aa2e
rafi-adly <rafiadly259@gmail.com>
2025-10-31 11:19:28 +0700

    Add files via upload

---
+++ Soal_3.sh
@@ -0,0 +1,11 @@
+cat > /etc/bind/named.conf.options <<'EOF'
+options {
+    directory "/var/cache/bind";
+    dnssec-validation no;
+    forwarders { 192.168.122.1; };
+    allow-query { any; };
+    auth-nxdomain no;
+    listen-on-v6 { any; };
+};
+EOF
+service bind9 restart
