40c99fccb13525c2040d82d2896d247c7ff92075
reva <reva.erica24@gmail.com>
2025-11-01 00:29:16 +0700

    Create Soal_1.sh

---
+++ Soal_1.sh
@@ -0,0 +1,35 @@
+# Durin
+
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE -s 10.88.0.0/16
+iptables -A FORWARD -i eth1 -o eth0 -j ACCEPT
+iptables -A FORWARD -i eth2 -o eth0 -j ACCEPT
+iptables -A FORWARD -i eth3 -o eth0 -j ACCEPT
+iptables -A FORWARD -i eth4 -o eth0 -j ACCEPT
+iptables -A FORWARD -i eth5 -o eth0 -j ACCEPT
+iptables -A FORWARD -i eth0 -o eth1 -j ACCEPT
+iptables -A FORWARD -i eth0 -o eth2 -j ACCEPT
+iptables -A FORWARD -i eth0 -o eth3 -j ACCEPT
+iptables -A FORWARD -i eth0 -o eth4 -j ACCEPT
+iptables -A FORWARD -i eth0 -o eth5 -j ACCEPT
+
+cat > /etc/default/isc-dhcp-relay << EOF
+SERVERS="10.88.4.2"  
+INTERFACES="eth1 eth2 eth3 eth4 eth5"
+OPTIONS=""
+EOF
+
+service isc-dhcp-relay restart
+
+# Gilgalad dan Amandil
+ip addr add 10.88.1.100/24 dev eth0
+ip link set eth0 up
+
+ip route add default via 10.88.1.1
+
+echo nameserver 192.168.122.1 > /etc/resolv.conf
+
+ping -c 3 10.88.1.1     
+ping -c 3 192.168.122.1 
+
+apt-get update
+apt-get install isc-dhcp-client -y
