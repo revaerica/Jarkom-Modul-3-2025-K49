0346fc69202315a6945d393334bd87fa0fa50dca
reva <reva.erica24@gmail.com>
2025-10-31 23:51:34 +0700

    Update Soal_2.sh

--- Soal_2.sh
+++ Soal_2.sh
@@ -1,5 +1,7 @@
 # DHCP Server (Aldarion)
+echo nameserver 192.168.122.1 > /etc/resolv.conf
 apt-get update && apt-get install isc-dhcp-server -y
+
 cat > /etc/default/isc-dhcp-server <<EOF
 INTERFACES="eth0"
 EOF
@@ -7,37 +9,42 @@ EOF
 cat > /etc/dhcp/dhcpd.conf <<EOF
 authoritative;
 max-lease-time 3600; 
-option domain-name-servers 10.88.1.3, 192.168.122.1;
+option domain-name-servers 10.88.5.2; 
+option domain-name "jarkomK49.com";
 
 subnet 10.88.1.0 netmask 255.255.255.0 {
-    range 10.88.1.6 10.88.1.34;
-    range 10.88.1.68 10.88.1.94;
-    option routers 10.88.1.1;
-    option broadcast-address 10.88.1.255;
-    # Client Manusia: setengah jam (1800 detik) [cite: 93]
-    default-lease-time 1800; 
+	range 10.88.1.6 10.88.1.34;
+	range 10.88.1.68 10.88.1.94;
+	option routers 10.88.1.1;
+	option broadcast-address 10.88.1.255;
+	default-lease-time 1800;
 }
 
 subnet 10.88.2.0 netmask 255.255.255.0 {
-    range 10.88.2.35 10.88.2.67;
-    range 10.88.2.96 10.88.2.121;
-    option routers 10.88.2.1;
-    option broadcast-address 10.88.2.255;
-    # Client Peri: seperenam jam (600 detik) [cite: 94]
-    default-lease-time 600; 
+	range 10.88.2.35 10.88.2.67;
+	range 10.88.2.96 10.88.2.121;
+	option routers 10.88.2.1;
+	option broadcast-address 10.88.2.255;
+	default-lease-time 600;
 }
 
 subnet 10.88.3.0 netmask 255.255.255.0 {
-    option routers 10.88.3.1;
-    option broadcast-address 10.88.3.255;
-    host Khamul {
-        hardware ethernet 02:42:92:bc:4c:00;
-        fixed-address 10.88.3.95;
-    }
+	option routers 10.88.3.1;
+	option broadcast-address 10.88.3.255;
+	host Khamul {
+		hardware ethernet 02:42:92:bc:4c:00;
+		fixed-address 10.88.3.95;
+	}
 }
 
 subnet 10.88.4.0 netmask 255.255.255.0 {
-    option routers 10.88.4.1;
+	option routers 10.88.4.1;
+	option broadcast-address 10.88.4.255;
+}
+
+subnet 10.88.5.0 netmask 255.255.255.0 {
+	option routers 10.88.5.1;
+	option broadcast-address 10.88.5.255;
 }
 EOF
 
