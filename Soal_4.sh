610e1a95526e9d2fa9635764f0a617a8e9352471
reva <reva.erica24@gmail.com>
2025-10-31 15:09:14 +0700

    Update Soal_4.sh

--- Soal_4.sh
+++ Soal_4.sh
@@ -1,24 +1,24 @@
 # Erendis ( ns1 / master )
 cat > /etc/bind/named.conf.local <<'EOF'
-zone "K49.com" {
+zone "jarkomK49.com" {
     type master;
     notify yes;
     also-notify { 10.88.3.3; };      // IP Amdir (ns2)
     allow-transfer { 10.88.3.3; };
-    file "/etc/bind/K49/K49.com";
+    file "/etc/bind/K49/jarkomK49.com";
 };
 EOF
-cat > /etc/bind/K49/K49.com<<'EOF'
+cat > /etc/bind/K49/jarkomK49.com<<'EOF'
 $TTL 604800
-@       IN SOA K49.com. root.K49.com. (
+@       IN SOA jarkomK49.com. root.jarkomK49.com. (
             2025101302 ; Serial YYYYMMDDXX
             604800     ; Refresh 1 minggu
             86400      ; Retry 1 hari
             2419200    ; Expire 4 minggu
             604800 )   ; Negative Cache TTL
 
-@         IN NS ns1.K49.com.
-@         IN NS ns2.K49.com.
+@         IN NS ns1.jarkomK49.com.
+@         IN NS ns2.jarkomK49.com.
 @         IN A 10.88.3.2
 
 ns1       IN A 10.88.3.2
@@ -33,5 +33,5 @@ Galadriel IN A 10.88.2.2
 Celeborn  IN A 10.88.2.3
 Oropher   IN A 10.88.2.4
 
-www       IN CNAME K49.com.
+www       IN CNAME jarkomK49.com.
 EOF
