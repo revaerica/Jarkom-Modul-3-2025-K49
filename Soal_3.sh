b324fa002c13545236c903268d3c81a8362404d1
reva <reva.erica24@gmail.com>
2025-11-01 02:41:44 +0700

    Update Soal_3.sh

--- Soal_3.sh
+++ Soal_3.sh
@@ -1,3 +1,4 @@
+# Minastir
 echo "nameserver 192.168.122.1" > /etc/resolv.conf
 
 ping -c 3 deb.debian.org
@@ -7,13 +8,16 @@ apt-get install bind9 bind9utils -y
 
 cat > /etc/bind/named.conf.options <<'EOF'
 options {
-    directory "/var/cache/bind";
-    dnssec-validation no;
-    forwarders { 192.168.122.1; };
-    allow-query { any; };
-    auth-nxdomain no;
-    listen-on-v6 { any; };
+    directory "/var/cache/bind";
+    dnssec-validation no;
+    forwarders { 192.168.122.1; };
+    allow-query { any; };
+    auth-nxdomain no;
+    listen-on-v6 { any; };
+    listen-on { 127.0.0.1; 10.88.5.2; };
 };
 EOF
 
-service bind9 restart
+pkill named
+/usr/sbin/named
+ps aux | grep named
