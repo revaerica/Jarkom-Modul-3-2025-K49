bfd129f5769f0249f6f8b82f207b120e9e1635c8
reva <reva.erica24@gmail.com>
2025-11-04 16:29:21 +0700

    Update Soal_9.sh

--- Soal_9.sh
+++ Soal_9.sh
@@ -17,25 +17,25 @@ echo ""
 echo "------------------------------------------"
 echo " TESTING ELENDIL (Worker 1 - Port 8001)"
 echo "------------------------------------------"
-lynx -dump http://10.88.4.2:8001 || echo "[!] Gagal mengakses halaman utama Elendil"
-curl -I http://10.88.4.2:8001
-curl http://10.88.4.2:8001/api/airing
+lynx -dump http://10.88.1.2:8001 || echo "[!] Gagal mengakses halaman utama Elendil"
+curl -I http://10.88.1.2:8001
+curl http://10.88.1.2:8001/api/airing
 echo ""
 
 echo "------------------------------------------"
 echo " TESTING ISILDUR (Worker 2 - Port 8002)"
 echo "------------------------------------------"
-lynx -dump http://10.88.4.3:8002 || echo "[!] Gagal mengakses halaman utama Isildur"
-curl -I http://10.88.4.3:8002
-curl http://10.88.4.3:8002/api/airing
+lynx -dump http://10.88.1.3:8002 || echo "[!] Gagal mengakses halaman utama Isildur"
+curl -I http://10.88.1.3:8002
+curl http://10.88.1.3:8002/api/airing
 echo ""
 
 echo "------------------------------------------"
 echo " TESTING ANARION (Worker 3 - Port 8003)"
 echo "------------------------------------------"
-lynx -dump http://10.88.4.4:8003 || echo "[!] Gagal mengakses halaman utama Anarion"
-curl -I http://10.88.4.4:8003
-curl http://10.88.4.4:8003/api/airing
+lynx -dump http://10.88.1.4:8003 || echo "[!] Gagal mengakses halaman utama Anarion"
+curl -I http://10.88.1.4:8003
+curl http://10.88.1.4:8003/api/airing
 echo ""
 
 echo "✅ Semua worker berhasil dites!"
